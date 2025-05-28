81f8a6f82e144c18f11d7d6f8cabd32c264b8f68
trafegoofertaecontingencia <juniorcms95@gmail.com>
2025-05-28 14:17:51 -0300

    logica do input alterada

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -66,7 +66,7 @@ export default function Form() {
 
       <div>
         <label className="block mb-1 font-medium">Categoria</label>
-        {formData.tipo === "receita" ? (
+        {formData.tipo === "despesa" ? (
           <select
             name="categoria"
             value={formData.categoria}
